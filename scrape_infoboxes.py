a5317884f28053f64768320e9a96d9a6b1be1b04
Ishika Agarwal <agarwali@purdue.edu>
2023-10-12 10:29:20 -0500

    update

--- scrape_infoboxes.py
+++ scrape_infoboxes.py
@@ -1,6 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 import os
+from tqdm import tqdm
+
+def get_soup(link_url):
+    response = requests.get(link_url)
+    if response.status_code == 200:
+        # Parse the HTML content of the page using BeautifulSoup
+        soup = BeautifulSoup(response.text, 'html.parser')
+        return soup
+    else:
+        print(f"Failed to retrieve the page. Status code: {response.status_code}")
+        0/0
+
 
 # Define the URL of the Wikipedia page you want to scrape
 urls = ['https://en.wikipedia.org/wiki/Category:Infobox_templates', 
@@ -16,28 +28,56 @@ urls = ['https://en.wikipedia.org/wiki/Category:Infobox_templates',
         'https://en.wikipedia.org/w/index.php?title=Category:Infobox_templates&pagefrom=windstorm+season%0AInfobox+windstorm+season#mw-pages']
 infobox_template_link_file = 'infobox_template_links.txt'
 
+prefix = 'https://en.wikipedia.org'
+infobox_templates_file = 'infobox_template_with_fields.txt'
+
 if not os.path.exists(infobox_template_link_file):
     for url in urls:
         # Send an HTTP GET request to the URL
-        response = requests.get(url)
+        soup = get_soup(url)
 
-        # Check if the request was successful
-        if response.status_code == 200:
-            # Parse the HTML content of the page using BeautifulSoup
-            soup = BeautifulSoup(response.text, 'html.parser')
+        # Find and extract all the links in the page
+        links = soup.find_all('a')
 
-            # Find and extract all the links in the page
-            links = soup.find_all('a')
+        # Print the links
+        for link in links:
+            href = link.get('href')
+            if href and href.startswith("/wiki/") and 'Template:Infobox' in href:
+                # Filter links to Wikipedia articles by checking the URL
+                with open(infobox_template_link_file, 'a+') as f:
+                    f.write(f'{href}\n')
+    print('scraped for infobox template links')
 
-            # Print the links
-            for link in links:
-                href = link.get('href')
-                if href and href.startswith("/wiki/") and 'Template:Infobox' in href:
-                    # Filter links to Wikipedia articles by checking the URL
-                    with open(infobox_template_link_file, 'a+') as f:
-                        f.write(f'{href}\n')
+if not os.path.exists(infobox_templates_file):
+    filter_text = "{{Infobox"
+    def get_fields(temp):
+        try:
+            x = temp.index(filter_text)
+            infobox_raw = temp[x: x+temp[x:].index("}}")]
+            infobox_raw = infobox_raw[infobox_raw.index("|"):]
+            infobox_raw = infobox_raw.split("|")
+            fields = []
+            for sep in infobox_raw:
+                if len(sep):
+                    sep = sep.strip().split(' ')[0]
+                    fields.append(sep)
+            return fields
+        except:
+            return x
+        
+    links = None
+    with open(infobox_template_link_file, 'r') as f:
+        links = f.readlines()
 
-        else:
-            print(f"Failed to retrieve the page. Status code: {response.status_code}")
-
-
+    for link in tqdm(links):
+        soup = get_soup(f'{prefix}{link[:-1]}')
+        temp = soup.get_text()
+        fields = get_fields(temp)
+        while(type(fields) is not list):
+            fields = get_fields(temp[fields+len(filter_text):])
+        
+        with open(infobox_templates_file, 'a+') as w:
+            w.write(f'{link[15:].strip()} {" ".join(fields)}\n')
+    print('scraped the fields of infobox templates')
+else:
+    print('try again')
